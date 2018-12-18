797d22528d2fc4f0ef82894e26783151d90a70eb
Wim Vandamme <wim.vandamme@tgbsf.com>
2018-12-18 08:02:33 +0100

    add cuda

--- gstreamer/build_gstreamer.bat
+++ gstreamer/build_gstreamer.bat
@@ -3,7 +3,7 @@
 set ROOT=%~dp0.
 
 echo installing gstreamer ...
-"%ROOT%\..\bin\unzip.exe" -q %ROOT%\gstreamer-1.14.4.zip -d %1
-move "%1\gstreamer-1.14.4\x86_64" "%1\gstreamer"
+"%ROOT%\..\bin\unzip.exe" -q %ROOT%\gstreamer-1.14.4.zip -d %ROOT%
+move "%ROOT%\gstreamer-1.14.4\x86_64" "%1\gstreamer"
 
 
