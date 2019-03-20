d80ef898ce29e3a71ceb8900d8d272dcae1fb840
Wim Vandamme <wim.vandamme@tgbsf.com>
2019-03-20 11:07:42 +0100

    add time

--- gstreamer/build_gstreamer.bat
+++ gstreamer/build_gstreamer.bat
@@ -4,6 +4,7 @@ set ROOT=%~dp0.
 
 echo installing gstreamer ...
 "%ROOT%\..\bin\unzip.exe" -q %ROOT%\gstreamer-1.14.4.zip -d %ROOT%
+timeout /t 2
 move "%ROOT%\gstreamer-1.14.4\x86_64" "%1\gstreamer"
 
 
